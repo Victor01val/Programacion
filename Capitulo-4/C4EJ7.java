48ef9047b844939c058d6c88cf5ec2f3e07dbb28
Victor01val <vvalolm1305@g.educaand.es>
2022-11-22 18:31:27 +0100

    Nuevo

---
+++ Capitulo-4/C4EJ7.java
@@ -0,0 +1,23 @@
+
+/*Víctor Valverde Olmedo
+ * Calcular media 
+ */
+import java.util.Scanner;
+
+public class C4EJ7 {
+  public static void main(String args[]) {
+    double num_uno = 0;
+    double num_dos = 0;
+    double num_tres = 0;
+    Scanner s = new Scanner(System.in);
+    System.out.println("Escribe el primer número");
+    num_uno = s.nextInt();
+    System.out.println("Escribe el segundo número");
+    num_dos = s.nextInt();
+    System.out.println("Escribe el tercer número");
+    num_tres = s.nextInt();
+    double suma = (num_uno + num_dos + num_tres) / 3;
+    System.out.println("La media es " + suma);
+    s.close(); // Evitar errores
+  }
+}
---
+++ Capitulo-4/C4EJ9.java
@@ -0,0 +1,53 @@
+/* Víctor Valverde Olmedo 1 DAW TARDES
+ * Resolver la ecuacion de segundo grado
+ */
+import java.util.Scanner;
+
+public class C4EJ9 {
+  public static void main(String args[]) {
+    int num_uno = 0;
+    int num_dos = 0;
+    int num_tres = 0;
+    int resultado = 0;
+    Scanner s = new Scanner(System.in);
+    {
+
+      System.out.println("Escribe el valor a");
+      num_uno = s.nextInt();
+      System.out.println("Escribe el valor b");
+      num_dos = s.nextInt();
+      System.out.println("Escribe el valor c");
+      num_tres = s.nextInt();
+      if ((num_uno == 0) && (num_dos == 0) && (num_tres == 0)) {
+        System.out.println("La ecuación tiene infinitas soluciones.");
+      }
+
+      if ((num_uno == 0) && (num_dos == 0) && (num_tres != 0)) {
+        System.out.println("La ecuación no tiene solución.");
+      }
+
+      if ((num_uno != 0) && (num_dos != 0) && (num_tres == 0)) {
+        System.out.println("x1 = 0");
+        System.out.println("x2 = " + (-num_dos / num_uno));
+      }
+
+      if ((num_uno == 0) && (num_dos != 0) && (num_tres != 0)) {
+        System.out.println("x1 = x2 = " + (-num_tres / num_dos));
+      }
+
+      if ((num_uno != 0) && (num_dos != 0) && (num_tres != 0)) {
+        resultado = num_dos * num_dos - (4 * num_uno * num_tres);
+
+        if (resultado < 0) {
+          System.out.println("La ecuación no tiene soluciones reales");
+        } else {
+
+          System.out.println("x1 = " + (-num_dos + Math.sqrt(resultado)) / (2 * num_uno));
+
+          System.out.println("x2 = " + (-num_dos - Math.sqrt(resultado)) / (2 * num_uno));
+          s.close();
+        }
+      }
+    }
+  }
+}
