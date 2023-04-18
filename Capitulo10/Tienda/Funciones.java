7379aeb30c0fdbf42867b5a1c496831b829b43eb
Victor01val <vvalolm1305@g.educaand.es>
2023-04-19 00:22:17 +0200

    Actualizado

---
+++ Capitulo10/Tienda/Funciones.java
@@ -0,0 +1,58 @@
+package Capitulo10.Tienda;
+
+public class Funciones {
+    
+    // Declaracion de variables
+  private int codigo;
+  private String descripcion;
+  private double precioCompra;
+  private double precioVenta;
+  private int stock;
+  // Lista de metodos
+  public int getCodigo() {
+    return this.codigo;
+  }
+
+  public void setCodigo(int codigo) {
+    this.codigo = codigo;
+  }
+
+  public String getdescripcion(String descripcion) {
+    return this.descripcion;
+  }
+
+  public void setdescripcion(String descripcion) {
+    this.descripcion = descripcion;
+  }
+
+  public double getPrecioCompra(double precioCompra) {
+    return this.precioCompra;
+  }
+                                                                            
+  public void setPrecioCompra(double precioCompra) {
+    this.precioCompra = precioCompra;
+  }
+
+  public double getPrecioVenta(double precioVenta) {
+    return this.precioVenta;
+  }
+
+  public void setPrecioVenta(double precioVenta) {
+    this.precioVenta = precioVenta;
+  }
+
+  public int getStock() {
+    return this.stock;
+  }
+
+  public void setStock(int stock) {
+    this.stock = stock;
+  }
+
+  public String toString() { //Funcion toString para imprimir los objetos
+
+    return "\nCódigo: " + this.codigo + "\nDescripción: " + this.descripcion + "\nPrecio de compra: "
+        + this.precioCompra + "\nPrecio de venta: " + this.precioVenta + "\nStock: " + this.stock + " unidades";
+
+  }
+}
--- Capitulo10/Tienda/Tienda.java
+++ Capitulo10/Tienda/Tienda.java
@@ -3,63 +3,11 @@
  * Tienda que muestra varias opciones, como listar los articulos, dar de alta, modificar, dar de baja, añadir y reducir mercancia, etc.
  */
 package Capitulo10.Tienda;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Tienda {
-    // Declaracion de variables
-  private int codigo;
-  private String descripcion;
-  private double precioCompra;
-  private double precioVenta;
-  private int stock;
-  // Lista de metodos
-  public int getCodigo() {
-    return this.codigo;
-  }
-
-  public void setCodigo(int codigo) {
-    this.codigo = codigo;
-  }
-
-  public String getdescripcion(String descripcion) {
-    return this.descripcion;
-  }
-
-  public void setdescripcion(String descripcion) {
-    this.descripcion = descripcion;
-  }
-
-  public double getPrecioCompra(double precioCompra) {
-    return this.precioCompra;
-  }
-                                                                            
-  public void setPrecioCompra(double precioCompra) {
-    this.precioCompra = precioCompra;
-  }
-
-  public double getPrecioVenta(double precioVenta) {
-    return this.precioVenta;
-  }
-
-  public void setPrecioVenta(double precioVenta) {
-    this.precioVenta = precioVenta;
-  }
-
-  public int getStock() {
-    return this.stock;
-  }
-
-  public void setStock(int stock) {
-    this.stock = stock;
-  }
-
-  public String toString() { //Funcion toString para imprimir los objetos
-
-    return "\nCódigo: " + this.codigo + "\nDescripción: " + this.descripcion + "\nPrecio de compra: "
-        + this.precioCompra + "\nPrecio de venta: " + this.precioVenta + "\nStock: " + this.stock + " unidades";
-
-  }
-
 
   public static void main(String args[]) {
     int codigo;
@@ -67,9 +15,8 @@ public class Tienda {
     double precioCompra;
     double precioVenta;
     int stock;
-  // Definicion de variables
-  ArrayList<Tienda> articulos = new ArrayList<Tienda>();
-   
+    // Definicion de variables
+    ArrayList<Funciones> articulos = new ArrayList<Funciones>();
 
     Scanner s = new Scanner(System.in);
     {
@@ -81,47 +28,47 @@ public class Tienda {
         n = s.nextInt();
         switch (n) {
           case 1:
-          for (Tienda art : articulos) {
-            if (art.getCodigo() != 0) {
+            for (Funciones art : articulos) {
+              if (art.getCodigo() != 0) {
                 System.out.println(art);
+              }
             }
-        }
-           
+
             break;
           case 2:
 
-          Tienda articuloNuevo = new Tienda(); //Creamos array
-          System.out.println("Introduzca los siguientes datos para dar de alta el producto:");
-          System.out.println("Codigo:");
-          codigo = s.nextInt();
-          articuloNuevo.setCodigo(codigo);
-          System.out.println("Descripcion:");
-          descripcion = System.console().readLine();
-          articuloNuevo.setdescripcion(descripcion);
-          System.out.println("Precio de compra:");
-          precioCompra = s.nextDouble();
-          articuloNuevo.setPrecioCompra(precioCompra);
-          System.out.println("Precio de venta:");
-          precioVenta = s.nextDouble();
-          articuloNuevo.setPrecioVenta(precioVenta);
-          System.out.println("Stock:");
-          stock = s.nextInt();
-          articuloNuevo.setStock(stock);
-          articulos.add(articuloNuevo); 
-          
-           
+            Funciones articuloNuevo = new Funciones(); // Creamos array
+            System.out.println("Introduzca los siguientes datos para dar de alta el producto:");
+            System.out.println("Codigo:");
+            codigo = s.nextInt();
+            articuloNuevo.setCodigo(codigo);
+            System.out.println("Descripcion:");
+            descripcion = System.console().readLine();
+            articuloNuevo.setdescripcion(descripcion);
+            System.out.println("Precio de compra:");
+            precioCompra = s.nextDouble();
+            articuloNuevo.setPrecioCompra(precioCompra);
+            System.out.println("Precio de venta:");
+            precioVenta = s.nextDouble();
+            articuloNuevo.setPrecioVenta(precioVenta);
+            System.out.println("Stock:");
+            stock = s.nextInt();
+            articuloNuevo.setStock(stock);
+            articulos.add(articuloNuevo);
+
             break;
           case 3:
-          System.out.println("Introduzca los siguientes datos para dar de baja el producto:");
-                    System.out.println("Codigo:");
-                    codigo = s.nextInt();
-                    for (Tienda articulo : articulos) {
-                        if (articulo.getCodigo() == codigo) { //El ArrayList recorre todo el array, si encuentra el codigo, lo retira
-                            articulos.remove(articulo);
-                           
-                        }
-                    }
-                    break;
+            System.out.println("Introduzca los siguientes datos para dar de baja el producto:");
+            System.out.println("Codigo:");
+            codigo = s.nextInt();
+            for (Funciones articulo : articulos) {
+              if (articulo.getCodigo() == codigo) { // El ArrayList recorre todo el array, si encuentra el codigo, lo
+                                                    // retira
+                articulos.remove(articulo);
+
+              }
+            }
+            break;
           case 4:
 
             do {
@@ -132,12 +79,13 @@ public class Tienda {
                 case 1:
                   System.out.println("Codigo:");
                   codigo = s.nextInt();
-                  for (Tienda articulo : articulos) {
-                    if (articulo.getCodigo() == codigo) { //El ArrayList recorre todo el array, si encuentra el codigo, modifica el codigo
-                        articulo.setCodigo(codigo);
-                       
+                  for (Funciones articulo : articulos) {
+                    if (articulo.getCodigo() == codigo) { // El ArrayList recorre todo el array, si encuentra el codigo,
+                                                          // modifica el codigo
+                      articulo.setCodigo(codigo);
+
                     }
-                }
+                  }
 
                   break;
                 case 2:
@@ -145,50 +93,54 @@ public class Tienda {
                   codigo = s.nextInt();
                   System.out.println("Escribe la descripción:");
                   descripcion = System.console().readLine();
-                  for (Tienda articulo : articulos) {
+                  for (Funciones articulo : articulos) {
                     if (articulo.getCodigo() == codigo) {
-                        articulo.setdescripcion(descripcion); //El ArrayList recorre todo el array, si encuentra el codigo, modifica la descripcion
-                       
+                      articulo.setdescripcion(descripcion); // El ArrayList recorre todo el array, si encuentra el
+                                                            // codigo, modifica la descripcion
+
                     }
-                }
+                  }
 
                   break;
                 case 3:
 
-                System.out.println("Escribe el codigo para identificar el producto:");
-                codigo = s.nextInt();
-                System.out.println("Escribe nuevo precio de compra:");
-                precioCompra = s.nextInt();
-                for (Tienda articulo : articulos) {
-                  if (articulo.getCodigo() == codigo) {
-                      articulo.setPrecioCompra(precioCompra); //El ArrayList recorre todo el array, si encuentra el codigo, modifica el precio
-                     
+                  System.out.println("Escribe el codigo para identificar el producto:");
+                  codigo = s.nextInt();
+                  System.out.println("Escribe nuevo precio de compra:");
+                  precioCompra = s.nextInt();
+                  for (Funciones articulo : articulos) {
+                    if (articulo.getCodigo() == codigo) {
+                      articulo.setPrecioCompra(precioCompra); // El ArrayList recorre todo el array, si encuentra el
+                                                              // codigo, modifica el precio
+
+                    }
                   }
-              }
-              break;
+                  break;
                 case 4:
-                System.out.println("Escribe el codigo para identificar el producto:");
-                codigo = s.nextInt();
-                System.out.println("Escribe nuevo precio de venta:");
-                precioVenta = s.nextInt();
-                for (Tienda articulo : articulos) {
-                  if (articulo.getCodigo() == codigo) {
-                      articulo.setPrecioVenta(precioVenta); //El ArrayList recorre todo el array, si encuentra el codigo, modifica el precio
-                     
+                  System.out.println("Escribe el codigo para identificar el producto:");
+                  codigo = s.nextInt();
+                  System.out.println("Escribe nuevo precio de venta:");
+                  precioVenta = s.nextInt();
+                  for (Funciones articulo : articulos) {
+                    if (articulo.getCodigo() == codigo) {
+                      articulo.setPrecioVenta(precioVenta); // El ArrayList recorre todo el array, si encuentra el
+                                                            // codigo, modifica el precio
+
+                    }
                   }
-              }
                   break;
                 case 5:
                   System.out.println("Escribe el codigo para identificar el producto:");
-                codigo = s.nextInt();
-                System.out.println("Escribe nuevo numero de stocks:");
-                stock = s.nextInt();
-                for (Tienda articulo : articulos) {
-                  if (articulo.getCodigo() == codigo) {
-                      articulo.setStock(stock); //El ArrayList recorre todo el array, si encuentra el codigo, modifica el stock
-                     
+                  codigo = s.nextInt();
+                  System.out.println("Escribe nuevo numero de stocks:");
+                  stock = s.nextInt();
+                  for (Funciones articulo : articulos) {
+                    if (articulo.getCodigo() == codigo) {
+                      articulo.setStock(stock); // El ArrayList recorre todo el array, si encuentra el codigo, modifica
+                                                // el stock
+
+                    }
                   }
-              }
                   break;
 
               }
@@ -196,30 +148,32 @@ public class Tienda {
 
             break;
           case 5:
-             System.out.println("Escribe el codigo para identificar el producto:");
-                codigo = s.nextInt();
-                System.out.println("Escribe la cantidad de stocks que quieres añadir:");
-                stock = s.nextInt();
-                for (Tienda articulo : articulos) {
-                  int nuevoStock = articulo.getStock() + stock;
-                  if (articulo.getCodigo() == codigo) {
-                    articulo.setStock(nuevoStock); //El ArrayList recorre todo el array, si encuentra el codigo, modifica el precio
-                     
-                  }
+            System.out.println("Escribe el codigo para identificar el producto:");
+            codigo = s.nextInt();
+            System.out.println("Escribe la cantidad de stocks que quieres añadir:");
+            stock = s.nextInt();
+            for (Funciones articulo : articulos) {
+              int nuevoStock = articulo.getStock() + stock;
+              if (articulo.getCodigo() == codigo) {
+                articulo.setStock(nuevoStock); // El ArrayList recorre todo el array, si encuentra el codigo, modifica
+                                               // el precio
+
               }
-              break;
-          case 6:
-          System.out.println("Escribe el codigo para identificar el producto:");
-          codigo = s.nextInt();
-          System.out.println("Escribe la cantidad de stocks que quieres retirar:");
-           stock = s.nextInt();
-          for (Tienda articulo : articulos) {
-            int nuevoStock = articulo.getStock() - stock;
-            if (articulo.getCodigo() == codigo) {
-                articulo.setStock(nuevoStock); //El ArrayList recorre todo el array, si encuentra el codigo, modifica el precio
-                
             }
-        }
+            break;
+          case 6:
+            System.out.println("Escribe el codigo para identificar el producto:");
+            codigo = s.nextInt();
+            System.out.println("Escribe la cantidad de stocks que quieres retirar:");
+            stock = s.nextInt();
+            for (Funciones articulo : articulos) {
+              int nuevoStock = articulo.getStock() - stock;
+              if (articulo.getCodigo() == codigo) {
+                articulo.setStock(nuevoStock); // El ArrayList recorre todo el array, si encuentra el codigo, modifica
+                                               // el precio
+
+              }
+            }
             break;
           case 7:
           default:
